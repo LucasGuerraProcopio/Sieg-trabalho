cb8bcdcf613c01a08c634933d01441d23630be2c
Gabriel Pereira do Nascimento <gabrielpnascimento.dev@gmail.com>
2026-09-10 12:35:27 -0300

    Versão final do bot de automação

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from time import * 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
-
+#entrar no site do login
+driver= webdriver.Chrome()
 driver.get("https://talkabit-z3eg.onrender.com/app/login")
+
+campo_login_id = driver.find_element(By.ID,"teamToken")
+campo_login_id.send_keys("MATRIX-KXGM6")
+
+campo_login_id = driver.find_element(By.ID,"password")
+campo_login_id.send_keys("talkabit")
+
+botao = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+botao.click()
 
 try: 
     linhas = driver.find_elements(By.CSS_SELECTOR, "#resultados table tbody tr")
